2817967f512a087f80f2cc3b6cf27cd1bbc20a7d
Te Taipo <hokioi-security@protonmail.ch>
2019-10-29 11:21:13 +1300

    Update face_encode.php

--- face_encode.php
+++ face_encode.php
@@ -1,5 +1,5 @@
 <?php
-include "class_crypto.php";
+include "class_aes.php";
 include "class_facesteg.php";
 
 header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
@@ -8,7 +8,7 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
-$crypto = new crypto( 256, 'CBC' );
+$AES = new AES( 256, 'CBC' );
 $detector = new Face_Steg('detection.dat',5);
 $image_name = 'Estella_01.png';
 $detector->face_detect( $image_name );
@@ -16,8 +16,8 @@ $detector->face_detect( $image_name );
 $secret_text = 'secret message';
 
 # do crypto
-$pass_code = $crypto->get_key( 86 ); // for the purposes of testing, autogen a hard passcode
-$cryptostring    = $crypto->do_crypto( $secret_text, $pass_code );
+$pass_code       = $AES->get_key( 86 ); // for the purposes of testing, autogen a hard passcode
+$cryptostring    = $AES->encrypt( $secret_text, $pass_code );
 
 # save the passcode and encrypted text somewhere
 $file_entry = "Image Name: ". $image_name . "\nPass Code: " . base64_encode( $pass_code ) . "\nEmbedded Encrypted String:\n" . $cryptostring;
