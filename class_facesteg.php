a55ceb028af6891d8a12e394a45844cfd5017c63
Te Taipo <hokioi-security@protonmail.ch>
2019-10-29 11:08:51 +1300

    Update class_facesteg.php

--- class_facesteg.php
+++ class_facesteg.php
@@ -25,12 +25,8 @@ class Face_Steg {
   protected $detection_data;
   protected $canvas;
   protected $face;
-  protected $hashType = 9;
-  protected $method;
   private $reduced_canvas;
   private $max_duration;
-  private $blocksize = 256;
-  private $mode = 'CBC';
 
   public function __construct($detection_file = 'detection.dat',$max_duration=5) {
     if (is_file($detection_file)) {
@@ -39,7 +35,6 @@ class Face_Steg {
       throw new Exception("Couldn't load detection data");
     }
     $this->max_duration = $max_duration;
-    $this->set_method( $this->blocksize, $this->mode);
   }
 
   public function face_detect($file) {
@@ -295,60 +290,6 @@ class Face_Steg {
       }
     }
     return true;
-  }
-  function do_crypto( $string, $key ) {
-    if ( false === $this->validate_params( $string ) ) throw new Exception( 'Invlid params!' );
-    $cipher_type    = $this->method;
-    $iv             = $this->get_iv();
-    $ciphertext_raw = ( false !== strpos( $this->method, 'GCM' ) ) ? trim( openssl_encrypt( $string, $cipher_type, $key, OPENSSL_RAW_DATA, $iv, $tag ) ) : trim( openssl_encrypt( $string, $cipher_type, $key, OPENSSL_RAW_DATA, $iv ) );
-    $hmac           = hash_hmac( 'sha256', $ciphertext_raw, $key, $as_binary = true );
-    $output         = base64_encode( $iv . $hmac . $ciphertext_raw );
-    return ( false !== strpos( $this->method, 'GCM' ) ) ? base64_encode( $tag ) . ',' . $output : $output;
-  }
-  function do_decrypto( $data, $key ) {
-    if ( false === $this->validate_params( $data ) ) throw new Exception( 'Invlid params!' );
-    $cipher_type        = $this->method;
-    if ( false !== strpos( $this->method, 'GCM' ) ) {
-      $get_data           = explode( ',', $data );
-      $tag                = base64_decode( $get_data[ 0 ] );
-      $ciphertext_dec     = base64_decode( $get_data[ 1 ] );
-    } else {
-      $ciphertext_dec     = base64_decode( $data );
-    }
-    $ivlen              = openssl_cipher_iv_length( $cipher = $cipher_type );
-    $iv                 = substr( $ciphertext_dec, 0, $ivlen );
-    $hmac               = substr( $ciphertext_dec, $ivlen, $sha2len = 32 );
-    $ciphertext_raw     = substr( $ciphertext_dec, $ivlen + $sha2len );
-    $original_plaintext = ( false !== strpos( $this->method, 'GCM' ) ) ? trim( openssl_decrypt( $ciphertext_raw, $cipher, $key, OPENSSL_RAW_DATA, $iv, $tag ) ) : trim( openssl_decrypt( $ciphertext_raw, $cipher, $key, OPENSSL_RAW_DATA, $iv ) );
-    $calcmac            = hash_hmac( 'sha256', $ciphertext_raw, $key, $as_binary = true );
-    if ( hash_equals( $hmac, $calcmac ) ) {
-      return $original_plaintext;
-    }
-  }
-  protected function get_iv() {
-       return openssl_random_pseudo_bytes( openssl_cipher_iv_length( $this->method ) );
-   }  
-  public function validate_params( $data ) {
-      if ( $data != null && $this->method != null ) {
-          return true;
-      } else {
-          return FALSE;
-      }
-  }
-  public function set_method( $blockSize = 256, $mode = 'CBC' ) {
-      if ( phpversion() > 7.0 ) $mode = 'GCM';
-      if ( phpversion() < 6 ) {
-        $this->method = null;
-        throw new Exception( 'Insecure version of PHP!' );
-      }
-      $this->method = 'AES-' . $blockSize . '-' . $mode;
-  }  
-  /**
-   * 
-   * @return cryptographically safe string of bytes
-   */
-  function get_key( $hashlen = 49 ) {
-    return openssl_random_pseudo_bytes( $hashlen );
   }
 }
 ?>
