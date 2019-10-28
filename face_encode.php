8b2238b07e5ed044654beb91f3f76afcc99eea22
Te Taipo <hokioi-security@protonmail.ch>
2019-10-28 21:52:20 +1300

    Update face_encode.php

--- face_encode.php
+++ face_encode.php
@@ -9,6 +9,21 @@ header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
 $detector = new Face_Steg('detection.dat',5);
-$detector->face_detect('Estella_01.png');
-$detector->toStegPNG('secret message');
+$image_name = 'Estella_01.png';
+$detector->face_detect( $image_name );
+
+$secret_text = 'secret message - test 1 2 3';
+
+# do crypto
+$pass_code = $detector->getHash( 86 ); // 512-bit
+$cryptostring    = $detector->do_crypto( $secret_text, $pass_code, 0, 0 );
+
+# save the passcode and encrypted text somewhere
+$file_entry = "Image Name: ". $image_name . "\nPass Code: " . $pass_code . "\nEmbedded Encrypted String:\n" . $cryptostring;
+$fp = fopen( 'passcode.txt', 'w' );
+fwrite( $fp, $file_entry );
+fclose( $fp );
+
+# create the image
+$detector->toStegPNG( $cryptostring );
 exit;
