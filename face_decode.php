7d1763c06f69492497fb3da771e533a33076d456
Te Taipo <hokioi-security@protonmail.ch>
2019-11-10 20:15:22 +1300

    Update face_decode.php

--- face_decode.php
+++ face_decode.php
@@ -7,12 +7,13 @@ header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
-header('Content-type: text/plain');
 
-$AES = new AES( 256, 'CBC' );
+use AES\AES;
+
 $detector = new Face_Steg('detection.dat',5);
 $detector->face_detect('Encoded_01.png');
-$key     = 'add pass code here';
-$decoded = $AES->decrypt( $detector->toStegMSG(), base64_decode( $key ) );
+$key     = 'grabthekeyfrom_passcode.txt';
+$a = 'feedfacedeadbeeffeedfacedeadbeefabaddad2'; // this is added for future use for example 2fa
+$decoded = AES::decrypt( $detector->toStegMSG(), base64_decode( $key ), $a );
 echo "Decoded MSG: " . ( ( strlen( $decoded  ) > 0 ) ? $decoded : 'Error: Incorrect Pass Code!' );
 exit;
