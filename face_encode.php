5e4e970c241a94147cc837d59a4bf0cc548db991
Te Taipo <hokioi-security@protonmail.ch>
2019-11-10 20:14:40 +1300

    Update face_encode.php

--- face_encode.php
+++ face_encode.php
@@ -8,19 +8,21 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
-$AES = new AES( 256, 'CBC' );
-$detector = new Face_Steg('detection.dat',5);
+use AES\AES;
+
+$detector = new Face_Steg( 'detection.dat',5 );
 $image_name = 'Estella_01.png';
 $detector->face_detect( $image_name );
 
-$secret_text = 'secret message';
+$secret_text = 'IP: ' . $_SERVER[ 'REMOTE_ADDR' ] . "\n";
 
 # do crypto
-$key             = $AES->get_key( 86 ); // for the purposes of testing, autogen a hard passcode
-$cryptostring    = $AES->encrypt( $secret_text, $key );
+$key             = AES::get_key( 256 ); // for the purposes of testing, autogen a hard passcode
+$a = 'feedfacedeadbeeffeedfacedeadbeefabaddad2'; // this is added for future use for example 2fa
+$cryptostring    = AES::encrypt( $secret_text, $key, $a );
 
 # save the passcode and encrypted text somewhere
-$file_entry = "Image Name: ". $image_name . "\nKey: " . base64_encode( $key ) . "\nEmbedded Encrypted String:\n" . $cryptostring;
+$file_entry = "Image Name: ". $image_name . "\nPass Code: " . base64_encode( $key ) . "\nEmbedded Encrypted String:\n" . $cryptostring;
 $fp = fopen( 'passcode.txt', 'w' );
 fwrite( $fp, $file_entry );
 fclose( $fp );
